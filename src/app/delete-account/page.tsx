945e69b092ee484ab3d0048c401be95e7b19713e
Germano Melo <germanomelo1@gmail.com>
2026-09-21 16:02:21 -0300

    ajustes

--- src/app/delete-account/page.tsx
+++ src/app/delete-account/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import DeleteAccountClient from './DeleteAccountClient';
+import DeleteAccountClient from '@/app/delete-account/DeleteAccountClient';
 
 export const metadata: Metadata = {
   title: 'Exclusão de Conta e Dados Pessoais | Siga',
